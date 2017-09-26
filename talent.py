e89d5e1a3be448537cd26626a370dc615b6b9c0a
Tim Herrmann <tim.herrmann@saitow.ag>
2017-09-26 18:10:01 +0200

    WIP: Label left from image

--- talent.py
+++ talent.py
@@ -4,18 +4,14 @@ from tkinter import *
 import pika
 from threading import Thread
 import simpleaudio as sa
-import sys
+
+
+
 
 #######################################################################
 # Consumer for RabbitMQ
 #######################################################################
 def amqp_consume():
-  # Play alarm sound
-  def play_alarm():
-    wave_obj = sa.WaveObject.from_wave_file("buzzer1.wav")
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-   
   # Callback on message
   def onmessage(ch, method, properties, body):
     body = body.decode('utf8')
@@ -24,17 +20,17 @@ def amqp_consume():
     if body == "joury0":
       print("COMMAND: alert jury0")
       set_alert("joury0")
-      #play_alarm()
+      play_alarm()
 
     elif body == "joury1":
       print("COMMAND: alert jury0")
       set_alert("joury1")
-      #play_alarm()
+      play_alarm()
 
     elif body == "joury2":
       print("COMMAND: alert jury0")
       set_alert("joury2")
-      #play_alarm()
+      play_alarm()
 
     elif body == "reset":
       print("COMMAND: reset")
@@ -62,6 +58,16 @@ def amqp_consume():
     amqp_connection.close()
 
 #######################################################################
+# Consumer for RabbitMQ
+#######################################################################
+def play_alarm():
+  print("DEBUG: Plaing alarm")
+  wave_obj = sa.WaveObject.from_wave_file("buzzer1.wav")
+  play_obj = wave_obj.play()
+#  play_obj.wait_done()
+
+
+#######################################################################
 # Hide and set alerts
 #######################################################################
 def set_alert(action):
@@ -69,22 +75,25 @@ def set_alert(action):
     pic_j0 = Label( root, bg="black", image=p_alert )
     lbl_j0 = Label( root, bg="black", fg="grey", text="Eva", font=("Helvetica", 24))
 
-    pic_j0.grid( row=0, column=0, padx='5', pady='5', sticky='ew' )
-    lbl_j0.grid( row=1, column=0, padx='5', pady='5', sticky='ew' )
+    pic_j0.grid( row=0, column=1, padx='5', pady='5', sticky='ew' )
+    lbl_j0.grid( row=0, column=0, padx='5', pady='5', sticky='ew' )
 
   elif action == "joury1":
     pic_j1 = Label( root, bg="black", image=p_alert )
     lbl_j1 = Label( root, bg="black", fg="grey", text="Fabian", font=("Helvetica", 24))
 
-    pic_j1.grid( row=0, column=1, padx='5', pady='5', sticky='ew' )
-    lbl_j1.grid( row=1, column=1, padx='5', pady='5', sticky='ew' )
+    pic_j1.grid( row=0, column=3, padx='5', pady='5', sticky='ew' )
+    lbl_j1.grid( row=0, column=2, padx='5', pady='5', sticky='ew' )
 
   elif action == "joury2":
     pic_j2 = Label( root, bg="black", image=p_alert )
     lbl_j2 = Label( root, bg="black", fg="grey", text="Tim", font=("Helvetica", 24))
 
-    pic_j2.grid( row=0, column=2, padx='5', pady='5', sticky='ew' )
-    lbl_j2.grid( row=1, column=2, padx='5', pady='5', sticky='ew' )
+    pic_j2.grid( row=0, column=5, padx='5', pady='5', sticky='ew' )
+    lbl_j2.grid( row=0, column=4, padx='5', pady='5', sticky='ew' )
+  
+  # Play alarm sound ;)
+  play_alarm()
 
 def reset_alert():
     pic_j0 = Label( root, bg="black", image=p_blank )
@@ -94,12 +103,12 @@ def reset_alert():
     lbl_j1 = Label( root, bg="black", fg="grey", text="", font=("Helvetica", 24))
     lbl_j2 = Label( root, bg="black", fg="grey", text="", font=("Helvetica", 24))
 
-    pic_j0.grid( row=0, column=0, padx='5', pady='5', sticky='ew' )
-    pic_j1.grid( row=0, column=1, padx='5', pady='5', sticky='ew' )
-    pic_j2.grid( row=0, column=2, padx='5', pady='5', sticky='ew' )
-    lbl_j0.grid( row=1, column=0, padx='5', pady='5', sticky='ew' )
-    lbl_j1.grid( row=1, column=1, padx='5', pady='5', sticky='ew' )
-    lbl_j2.grid( row=1, column=2, padx='5', pady='5', sticky='ew' )
+    pic_j0.grid( row=0, column=1, padx='5', pady='5', sticky='ew' )
+    pic_j1.grid( row=0, column=3, padx='5', pady='5', sticky='ew' )
+    pic_j2.grid( row=0, column=5, padx='5', pady='5', sticky='ew' )
+    lbl_j0.grid( row=0, column=0, padx='5', pady='5', sticky='ew' )
+    lbl_j1.grid( row=0, column=2, padx='5', pady='5', sticky='ew' )
+    lbl_j2.grid( row=0, column=4, padx='5', pady='5', sticky='ew' )
 
 
 #######################################################################
@@ -115,6 +124,7 @@ def btn_set_alert_j2():
 def btn_reset_alert():
   reset_alert()
 
+
 #######################################################################
 # Main 
 #######################################################################
@@ -127,12 +137,15 @@ stand_alone="true"
 # Create main GUI object
 root = Tk()
 root.configure(background='black')
+root.title("ZBD got Talent")
 
 # Configure Layout
-root.columnconfigure(0, weight=5)
-root.columnconfigure(1, weight=0)
-root.columnconfigure(2, weight=5)
-
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+root.columnconfigure(3, weight=1)
+root.columnconfigure(4, weight=1)
+root.columnconfigure(5, weight=1)
 ###################################
 # Load pictures
 p_alert = PhotoImage(file="alert.png")
@@ -150,13 +163,13 @@ else:
   b_j0_set = Button( root, text="Set Alert", command=btn_set_alert_j0 )
   b_j1_set = Button( root, text="Set Alert", command=btn_set_alert_j1 )
   b_j2_set = Button( root, text="Set Alert", command=btn_set_alert_j2 )
-  b_j0_set.grid( row=2, column=0 )
-  b_j1_set.grid( row=2, column=1 )
-  b_j2_set.grid( row=2, column=2 )
+  b_j0_set.grid( row=2, column=1 )
+  b_j1_set.grid( row=2, column=3 )
+  b_j2_set.grid( row=2, column=5 )
   
   # hide alert button for testing
   b_hide = Button( root, text="Clear Alert", command=btn_reset_alert )
-  b_hide.grid( column=1, row=3 )
+  b_hide.grid( column=3, row=3 )
   
   # Testlabel
   #ltest = Label( root, text="wurst" )
