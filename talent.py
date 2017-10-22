97a15edf804696a4bd5737f4295041dd8005b8aa
Tim Herrmann <herrmann.tim92@gmail.com>
2017-10-22 23:45:55 +0200

    Reformatted code for matching python styleguide

--- talent.py
+++ talent.py
@@ -5,101 +5,109 @@ import pika
 from threading import Thread
 import simpleaudio as sa
 
+
 #######################################################################
 # Consumer for RabbitMQ
 def amqp_consume():
-  # Callback on message
-  def onmessage(ch, method, properties, body):
-    body = body.decode('utf8')
+    # Callback on message
+    def onmessage(ch, method, properties, body):
+        body = body.decode('utf8')
 
-    if body == "jury0":
-      print("[INFO] Alert jury0")
-      set_alert("jury0")
+        if body == "jury0":
+            print("[INFO] Alert jury0")
+            set_alert("jury0")
 
-    elif body == "jury1":
-      print("[INFO] Alert jury1")
-      set_alert("jury1")
+        elif body == "jury1":
+            print("[INFO] Alert jury1")
+            set_alert("jury1")
 
-    elif body == "jury2":
-      print("[INFO] Alert jury2")
-      set_alert("jury2")
+        elif body == "jury2":
+            print("[INFO] Alert jury2")
+            set_alert("jury2")
 
-    elif body == "reset":
-      print("[INFO] Reset")
-      reset_alert()
+        elif body == "reset":
+            print("[INFO] Reset")
+            reset_alert()
 
-    else:
-      print("[INFO] UNKNOWN")
+        else:
+            print("[INFO] UNKNOWN")
 
-    # Newline for nicer looking
-    print()
+        # Newline for nicer looking
+        print()
 
-  # Main of amqp_consume
-  amqp_credentials = pika.PlainCredentials('buzzer', 'buzzer')
-  amqp_parameter = pika.ConnectionParameters('192.168.0.127', 5672, '/', amqp_credentials)
-  amqp_connection = pika.BlockingConnection(amqp_parameter)
-  amqp_channel = amqp_connection.channel()
+    # Main of amqp_consume
+    amqp_credentials = pika.PlainCredentials('buzzer', 'buzzer')
+    amqp_parameter = pika.ConnectionParameters(
+        '192.168.0.127', 5672, '/', amqp_credentials)
+    amqp_connection = pika.BlockingConnection(amqp_parameter)
+    amqp_channel = amqp_connection.channel()
 
-  amqp_channel.queue_declare(queue='buzzer', auto_delete=True)
-  amqp_channel.basic_consume(onmessage, queue='buzzer', no_ack=True)
+    amqp_channel.queue_declare(queue='buzzer', auto_delete=True)
+    amqp_channel.basic_consume(onmessage, queue='buzzer', no_ack=True)
 
-  try:
-    amqp_channel.start_consuming()
-  except KeyboardInterrupt:
-    amqp_channel.stop_consuming()
-    amqp_connection.close()
+    try:
+        amqp_channel.start_consuming()
+    except KeyboardInterrupt:
+        amqp_channel.stop_consuming()
+        amqp_connection.close()
 
 
 #######################################################################
 # Play buzzer-sound
 def play_alarm():
-  print("[INFO] Playing sound")
-  wave_obj = sa.WaveObject.from_wave_file("sounds/buzzer1.wav")
-  play_obj = wave_obj.play()
-  play_obj.wait_done()
+    print("[INFO] Playing sound")
+    wave_obj = sa.WaveObject.from_wave_file("sounds/buzzer1.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
 
 
 #######################################################################
 # Set alerts and call buzzer-sound routine
 def set_alert(action):
-  if action == "jury0":
-    pic_j0 = Label( root, bg="black", image=pic_alert_j0 )
-    pic_j0.grid( row=0, column=0, padx='0', pady='0' )
-    play_alarm() # call buzzer-sound routine
+    if action == "jury0":
+        pic_j0 = Label(root, bg="black", image=pic_alert_j0)
+        pic_j0.grid(row=0, column=0, padx='0', pady='0')
+        play_alarm()  # call buzzer-sound routine
 
-  elif action == "jury1":
-    pic_j1 = Label( root, bg="black", image=pic_alert_j1 )
-    pic_j1.grid( row=0, column=1, padx='0', pady='0' )
-    play_alarm() # call buzzer-sound routine
+    elif action == "jury1":
+        pic_j1 = Label(root, bg="black", image=pic_alert_j1)
+        pic_j1.grid(row=0, column=1, padx='0', pady='0')
+        play_alarm()  # call buzzer-sound routine
 
-  elif action == "jury2":
-    pic_j2 = Label( root, bg="black", image=pic_alert_j2 )
-    pic_j2.grid( row=0, column=2, padx='0', pady='0' )
-    play_alarm() # call buzzer-sound routine
+    elif action == "jury2":
+        pic_j2 = Label(root, bg="black", image=pic_alert_j2)
+        pic_j2.grid(row=0, column=2, padx='0', pady='0')
+        play_alarm()  # call buzzer-sound routine
 
 
 #######################################################################
 # Reset alerts
 def reset_alert():
-    pic_j0 = Label( root, bg="black", image=pic_blank_j0 )
-    pic_j1 = Label( root, bg="black", image=pic_blank_j1 )
-    pic_j2 = Label( root, bg="black", image=pic_blank_j2 )
+    pic_j0 = Label(root, bg="black", image=pic_blank_j0)
+    pic_j1 = Label(root, bg="black", image=pic_blank_j1)
+    pic_j2 = Label(root, bg="black", image=pic_blank_j2)
 
-    pic_j0.grid( row=0, column=0, padx='5', pady='5' )
-    pic_j1.grid( row=0, column=1, padx='5', pady='5' )
-    pic_j2.grid( row=0, column=2, padx='5', pady='5' )
+    pic_j0.grid(row=0, column=0, padx='5', pady='5')
+    pic_j1.grid(row=0, column=1, padx='5', pady='5')
+    pic_j2.grid(row=0, column=2, padx='5', pady='5')
 
 
 #######################################################################
 # Button actions
 def btn_set_alert_j0():
-  set_alert("jury0")
+    set_alert("jury0")
+
+
 def btn_set_alert_j1():
-  set_alert("jury1")
+    set_alert("jury1")
+
+
 def btn_set_alert_j2():
-  set_alert("jury2")
+    set_alert("jury2")
+
+
 def btn_reset_alert():
-  reset_alert()
+    reset_alert()
 
 
 #######################################################################
@@ -108,7 +116,7 @@ def btn_reset_alert():
 
 ###################################
 # Configuration parameter
-stand_alone="false"
+stand_alone = "false"
 
 ###################################
 # Create main GUI object
@@ -137,23 +145,23 @@ reset_alert()
 ###################################
 # Start RabbitMQ-Consumer or use buttons for simulation
 if stand_alone == "false":
-  t1 = Thread( target=amqp_consume )
+    t1 = Thread(target=amqp_consume)
 #  t1.saemon = True
-  t1.start()
+    t1.start()
 else:
-  b_j0_set = Button( root, text="Set Alert", command=btn_set_alert_j0 )
-  b_j1_set = Button( root, text="Set Alert", command=btn_set_alert_j1 )
-  b_j2_set = Button( root, text="Set Alert", command=btn_set_alert_j2 )
-  b_j0_set.grid( row=1, column=0 )
-  b_j1_set.grid( row=1, column=1 )
-  b_j2_set.grid( row=1, column=2 )
+    b_j0_set = Button(root, text="Set Alert", command=btn_set_alert_j0)
+    b_j1_set = Button(root, text="Set Alert", command=btn_set_alert_j1)
+    b_j2_set = Button(root, text="Set Alert", command=btn_set_alert_j2)
+    b_j0_set.grid(row=1, column=0)
+    b_j1_set.grid(row=1, column=1)
+    b_j2_set.grid(row=1, column=2)
 
-  # hide alert button for testing
-  b_hide = Button( root, text="Clear Alert", command=btn_reset_alert )
-  b_hide.grid( row=2, column=1 )
+    # hide alert button for testing
+    b_hide = Button(root, text="Clear Alert", command=btn_reset_alert)
+    b_hide.grid(row=2, column=1)
 
 # Start main GUI
 try:
-  root.mainloop()
+    root.mainloop()
 except KeyboardInterrupt:
-  exit
+    exit
--- talent_raspberry.py
+++ talent_raspberry.py
@@ -3,58 +3,63 @@ import RPi.GPIO as GPIO
 import pika
 import time
 
-bouncetime=300
+bouncetime = 300
 
 GPIO.setmode(GPIO.BCM)
 
-#GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(27, GPIO.IN)
 GPIO.setup(22, GPIO.IN)
 
+
 def send_message(message):
-  connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-  channel = connection.channel()
-  channel.basic_publish(exchange='', routing_key='buzzer', body=message)
-  connection.close()
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.basic_publish(exchange='', routing_key='buzzer', body=message)
+    connection.close()
 
 
 def button1(channel):
-  global count_j0
-  count_j0+=1
-  print ("[INFO] Alarm Jury0:", count_j0)
-  send_message("jury0")
+    global count_j0
+    count_j0 += 1
+    print("[INFO] Alarm Jury0:", count_j0)
+    send_message("jury0")
+
 
 def button2(channel):
-  global count_j1
-  count_j1+=1
-  print ("[INFO] Alarm Jury1:", count_j1)
-  send_message("jury1")
+    global count_j1
+    count_j1 += 1
+    print("[INFO] Alarm Jury1:", count_j1)
+    send_message("jury1")
+
 
 def button3(channel):
-  global count_j2
-  count_j2+=1
-  print ("[INFO] Alarm Jury2:", count_j2)
-  send_message("jury2")
+    global count_j2
+    count_j2 += 1
+    print("[INFO] Alarm Jury2:", count_j2)
+    send_message("jury2")
+
 
 GPIO.add_event_detect(17, GPIO.FALLING, callback=button1, bouncetime=100)
 GPIO.add_event_detect(27, GPIO.FALLING, callback=button2, bouncetime=100)
 GPIO.add_event_detect(22, GPIO.FALLING, callback=button3, bouncetime=100)
 
-#GPIO.add_event_detect(17, GPIO.FALLING, callback=button1)
-#GPIO.add_event_detect(27, GPIO.FALLING, callback=button2)
-#GPIO.add_event_detect(22, GPIO.FALLING, callback=button3)
+# GPIO.add_event_detect(17, GPIO.FALLING, callback=button1)
+# GPIO.add_event_detect(27, GPIO.FALLING, callback=button2)
+# GPIO.add_event_detect(22, GPIO.FALLING, callback=button3)
 
-count_j0=0
-count_j1=0
-count_j2=0
+count_j0 = 0
+count_j1 = 0
+count_j2 = 0
 
 try:
-  time.sleep(999999)
+    time.sleep(999999)
 
 except KeyboardInterrupt:
-  GPIO.cleanup() # clean up GPIO on CTRL+C exit
+    GPIO.cleanup()  # clean up GPIO on CTRL+C exit
 GPIO.cleanup()   # clean up GPIO on normal exit
